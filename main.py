bb88210a4061aa4241a0fa621fa24eb9854e5af9
二刺螈 <i@songe.li>
2025-02-15 15:16:09 +0800

    feat: update

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import vgamepad as vg
 import time
-
-gamepad = vg.VX360Gamepad()
+import keyboard
 
 print("macropad started")
+
+active = False
+gamepad: vg.VX360Gamepad | None = None
+
+
+def swtch_state():
+    global active
+    active = not active
+
+
+keyboard.add_hotkey("j", swtch_state)
+print("press 'j' to switch gamepad on/off")
+
 try:
     while True:
-        gamepad.press_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_X)
-        gamepad.update()
-        time.sleep(0.1)
-        gamepad.release_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_X)
-        gamepad.update()
-        time.sleep(2)
+        if active:
+            if gamepad is None:
+                gamepad = vg.VX360Gamepad()
+                print("gamepad on")
+        else:
+            if gamepad is not None:
+                del gamepad
+                gamepad = None
+                print("gamepad off")
+        if gamepad is not None:
+            gamepad.press_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_X)
+            gamepad.update()
+            time.sleep(0.1)
+            gamepad.release_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_X)
+            gamepad.update()
+        time.sleep(2.5)
 except KeyboardInterrupt:
     pass
 
-del gamepad
+if gamepad is not None:
+    del gamepad
+
 print("macropad end")
